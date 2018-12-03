33e7bb91e3acace11ac03b94aadabbcb63e2d693
Justin Lee <justinlee0022@gmail.com>
2018-12-03 09:14:35 -0800

    Modified service worker

--- sw.js
+++ sw.js
@@ -1,330 +1,75 @@
-const CACHE_VERSION = 1;
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-const BASE_CACHE_FILES = [
-  '/style.css',
-  '/script.js',
-  '/search.json',
-  '/manifest.json',
-  '/favicon.png',
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  'styles.css',
+  '../../styles/main.css',
+  'demo.js'
 ];
 
-const OFFLINE_CACHE_FILES = [
-  '/style.css',
-  '/script.js',
-  '/offline/index.html',
-];
-
-const NOT_FOUND_CACHE_FILES = [
-  '/style.css',
-  '/script.js',
-  '/404.html',
-];
-
-const OFFLINE_PAGE = '/offline/index.html';
-const NOT_FOUND_PAGE = '/404.html';
-
-const CACHE_VERSIONS = {
-  assets: 'assets-v' + CACHE_VERSION,
-  content: 'content-v' + CACHE_VERSION,
-  offline: 'offline-v' + CACHE_VERSION,
-  notFound: '404-v' + CACHE_VERSION,
-};
-
-// Define MAX_TTL's in SECONDS for specific file extensions
-const MAX_TTL = {
-  '/': 3600,
-  html: 3600,
-  json: 86400,
-  js: 86400,
-  css: 86400,
-};
-
-const CACHE_BLACKLIST = [
-  //(str) => {
-  //    return !str.startsWith('http://localhost') && !str.startsWith('https://gohugohq.com');
-  //},
-];
-
-const SUPPORTED_METHODS = [
-  'GET',
-];
-
-/**
- * isBlackListed
- * @param {string} url
- * @returns {boolean}
- */
-function isBlacklisted(url) {
-  return (CACHE_BLACKLIST.length > 0) ? !CACHE_BLACKLIST.filter((rule) => {
-    if (typeof rule === 'function') {
-      return !rule(url);
-    } else {
-      return false;
-    }
-  }).length : false
-}
-
-/**
- * getFileExtension
- * @param {string} url
- * @returns {string}
- */
-function getFileExtension(url) {
-  let extension = url.split('.').reverse()[0].split('?')[0];
-  return (extension.endsWith('/')) ? '/' : extension;
-}
-
-/**
- * getTTL
- * @param {string} url
- */
-function getTTL(url) {
-  if (typeof url === 'string') {
-    let extension = getFileExtension(url);
-    if (typeof MAX_TTL[extension] === 'number') {
-      return MAX_TTL[extension];
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
-}
-
-/**
- * installServiceWorker
- * @returns {Promise}
- */
-function installServiceWorker() {
-  return Promise.all(
-    [
-      caches.open(CACHE_VERSIONS.assets)
-        .then(
-          (cache) => {
-            return cache.addAll(BASE_CACHE_FILES);
-          }
-        ),
-      caches.open(CACHE_VERSIONS.offline)
-        .then(
-          (cache) => {
-            return cache.addAll(OFFLINE_CACHE_FILES);
-          }
-        ),
-      caches.open(CACHE_VERSIONS.notFound)
-        .then(
-          (cache) => {
-            return cache.addAll(NOT_FOUND_CACHE_FILES);
-          }
-        )
-    ]
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
-}
-
-/**
- * cleanupLegacyCache
- * @returns {Promise}
- */
-function cleanupLegacyCache() {
-
-  let currentCaches = Object.keys(CACHE_VERSIONS)
-    .map(
-      (key) => {
-        return CACHE_VERSIONS[key];
-      }
-    );
-
-  return new Promise(
-    (resolve, reject) => {
-
-      caches.keys()
-        .then(
-          (keys) => {
-            return legacyKeys = keys.filter(
-              (key) => {
-                return !~currentCaches.indexOf(key);
-              }
-            );
-          }
-        )
-        .then(
-          (legacy) => {
-            if (legacy.length) {
-              Promise.all(
-                legacy.map(
-                  (legacyKey) => {
-                    return caches.delete(legacyKey)
-                  }
-                )
-              )
-                .then(
-                  () => {
-                    resolve()
-                  }
-                )
-                .catch(
-                  (err) => {
-                    reject(err);
-                  }
-                );
-            } else {
-              resolve();
-            }
-          }
-        )
-        .catch(
-          () => {
-            reject();
-          }
-        );
-
-    }
-  );
-}
-
-
-self.addEventListener(
-  'install', event => {
-    event.waitUntil(installServiceWorker());
-  }
-);
+});
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener(
-  'activate', event => {
-    event.waitUntil(
-      Promise.all(
-        [
-          cleanupLegacyCache(),
-        ]
-      )
-        .catch(
-          (err) => {
-            event.skipWaiting();
-          }
-        )
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
     );
   }
-);
-
-self.addEventListener(
-  'fetch', event => {
-
-    event.respondWith(
-      caches.open(CACHE_VERSIONS.content)
-        .then(
-          (cache) => {
-
-            return cache.match(event.request)
-              .then(
-                (response) => {
-
-                  if (response) {
-
-                    let headers = response.headers.entries();
-                    let date = null;
-
-                    for (let pair of headers) {
-                      if (pair[0] === 'date') {
-                        date = new Date(pair[1]);
-                      }
-                    }
-
-                    if (date) {
-                      let age = parseInt((new Date().getTime() - date.getTime()) / 1000);
-                      let ttl = getTTL(event.request.url);
-
-                      if (ttl & amp;& amp; age > ttl) {
-
-              return new Promise(
-                (resolve) => {
-
-                  return fetch(event.request)
-                    .then(
-                      (updatedResponse) => {
-                        if (updatedResponse) {
-                          cache.put(event.request, updatedResponse.clone());
-                          resolve(updatedResponse);
-                        } else {
-                          resolve(response)
-                        }
-                      }
-                    )
-                    .catch(
-                      () => {
-                        resolve(response);
-                      }
-                    );
-
-                }
-              )
-                .catch(
-                  (err) => {
-                    return response;
-                  }
-                );
-            } else {
-              return response;
-            }
-
-          } else {
-            return response;
-          }
-
-                                    } else {
-    return null;
-  }
-                                }
-)
-  .then(
-    (response) => {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request)
-          .then(
-            (response) => {
-
-              if (response.status < 400) {
-                if (~SUPPORTED_METHODS.indexOf(event.request.method) & amp;& amp; !isBlacklisted(event.request.url)) {
-          cache.put(event.request, response.clone());
-        }
-        return response;
-      } else {
-        return caches.open(CACHE_VERSIONS.notFound).then((cache) => {
-          return cache.match(NOT_FOUND_PAGE);
-        })
-      }
-    }
-  )
-  .then((response) => {
-    if (response) {
-      return response;
-    }
-  })
-  .catch(
-    () => {
-
-      return caches.open(CACHE_VERSIONS.offline)
-        .then(
-          (offlineCache) => {
-            return offlineCache.match(OFFLINE_PAGE)
-          }
-        )
-
-    }
-  );
-                                    }
-                                }
-                            )
-                            .catch (
-                                (error) => {
-  console.error('  Error in fetch handler:', error);
-  throw error;
-}
-                            );
-                    }
-                )
-        );
-
-    }
-);
+});
